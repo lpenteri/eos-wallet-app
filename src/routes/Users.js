c73d2262b29d6ad69d0e1ce934fa7356acc6e096
stubelshe <stu.belshe@tandem.ly>
2017-08-17 14:31:26 -0500

    Styles for users page

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react'
 import {Users as UsersContainer} from '../containers'
 
 class Users extends Component {
-  render () {
-    return (
-      <div>
-        <h2>Users</h2>
-        <UsersContainer />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+                <div className="container-full">
+                    <div className="d-flex justify-content-end">
+                        <div className="mr-auto align-self-center">
+                            <h2>Users</h2>
+                        </div>
+                        <div className="align-self-center">
+                            <div className="module-search">
+                              <input type="text" className="form-control"></input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+				            <UsersContainer />
+                        </div>
+                    </div>
+                </div>
+			</div>
+		)
+	}
 }
 
 export default Users
