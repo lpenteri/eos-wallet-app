4da5a0a2187cc4093ff7595a345ffc38f1bb5f03
stubelshe <stu.belshe@tandem.ly>
2017-08-17 09:20:23 -0500

    Renaming of styles

--- src/containers/Transactions.js
+++ src/containers/Transactions.js
@@ -4,23 +4,23 @@ import {List, Icon} from '../components'
 import TransactionsQuery from '../query/transactions'
 
 const Transaction = ({ date, sender, memo, amount }) => (
-	<div className="-list-item -transaction d-flex flex-row">
-		<div className="-transaction-left d-flex flex-row">
-			<div className="-transaction-date">
+	<div className="transaction d-flex flex-row">
+		<div className="transaction-meta d-flex flex-row">
+			<div className="transaction-date">
 				<div className="date-month">{date.month}</div>
 				<div className="date-day">{date.day}</div>
 			</div>
 
-			<Icon className="-thumbnail" url={sender.icon} />
+			<Icon className="transaction-thumbnail" url={sender.icon} />
 
-			<div className="-transaction-memo">
+			<div className="transaction-memo">
                 <p>{memo}</p>
             </div>
 		</div>
 
-		<div className="-transaction-amount-container d-flex flex-row-reverse">
-			<Icon className="-transaction-icon icon-eos_icons_transfer_to" />
-            <p className="-transaction-amount">{amount}</p>
+		<div className="transaction-amount-container d-flex flex-row-reverse">
+			<Icon className="transaction-icon icon-eos_icons_transfer_to" />
+            <p className="transaction-amount">{amount}</p>
 		</div>
 	</div>
 )
