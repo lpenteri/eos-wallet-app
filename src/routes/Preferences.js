de5e7f7e0d4d9482e065c7c1decfd5a1e2833b47
Julien Heller <fluxyapparel@gmail.com>
2017-08-18 18:05:05 -0400

    Implement titles via Helmet

--- src/routes/Preferences.js
+++ src/routes/Preferences.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { ResetPasswordForm } from '../components';
+import { Helmet } from 'react-helmet';
 
 class Preferences extends Component {
   render() {
     return (
       <div>
+        <Helmet>
+          <title>Preferences</title>
+        </Helmet>
         <h2>Preferences</h2>
         <ResetPasswordForm />
       </div>
