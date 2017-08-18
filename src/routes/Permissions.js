ea152390f91bc7d664dbb6b4acc7aba3f3ae67c3
stubelshe <stu.belshe@tandem.ly>
2017-08-18 11:27:22 -0500

    Permissions view styles

--- src/routes/Permissions.js
+++ src/routes/Permissions.js
@@ -6,38 +6,50 @@ class Permissions extends Component {
     return (
       <div>
         <h2>Permissions</h2>
-        <h3>Keystore</h3>
+        <div className="section">
+          <h4>Keystore</h4>
 
-        <div className="keystore">
-          <p>Active</p>
-          <div>
-            <img
-              alt=""
-              src=""
-            />
-            <p>IEfja89uewaejwfawe0fjaewf98hwf8ejfhefwfebfaiuewfeuifh</p>
-            <button>Show Private Key</button>
+          <div className="keystore">
+            <h6>Active</h6>
+            <div className="list-item d-flex flex-row justify-content-between items-center">
+              <div>
+                <img
+                  alt=""
+                  src=""
+                />
+                <span>IEfja89uewaejwfawe0fjaewf98hwf8ejfhefwfebfaiuewfeuifh</span>
+              </div>
+              <button className="btn btn-primary btn-xsm">Show Private Key</button>
+            </div>
+
+            <p>The active key is used to make transfers...</p>
           </div>
 
-          <p>The active key is used to make transfers...</p>
-        </div>
+          <div className="keystore">
+            <h6>Owner</h6>
+            <div className="list-item d-flex flex-row justify-content-between items-center">
+              <div>
+                <img
+                  alt=""
+                  src=""
+                />
+                <span>IEfja89uewaejwfawe0fjaewf98hwf8ejfhefwfebfaiuewfeuifh</span>
+              </div>
+              <button className="btn btn-primary btn-xsm">Show Private Key</button>
+            </div>
 
-        <div className="keystore">
-          <p>Owner</p>
-          <div>
-            <img
-              alt=""
-              src=""
-            />
-            <p>IEfja89uewaejwfawe0fjaewf98hwf8ejfhefwfebfaiuewfeuifh</p>
-            <button>Show Private Key</button>
+            <p>The owner key is the master key...<br />The private key or password for the owner..</p>
           </div>
-
-          <p>The owner key is the master key...</p>
-          <p>The private key or password for the owner..</p>
         </div>
-
-        <ResetPasswordForm />
+      
+        <div className="section">
+          <h3>Reset Password</h3>
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <ResetPasswordForm />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
