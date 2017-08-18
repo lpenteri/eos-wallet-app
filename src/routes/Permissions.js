b6bb668e2af59a36ee1ed4158b0170cc2828d287
Orlando La <ceanela@users.noreply.github.com>
2017-08-18 10:00:28 -0500

    Add markup, copy and forms to <Permissions />

--- src/routes/Permissions.js
+++ src/routes/Permissions.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import { ResetPasswordForm } from '../components';
 
 class Permissions extends Component {
   render() {
     return (
       <div>
         <h2>Permissions</h2>
+        <h3>Keystore</h3>
+
+        <div className="keystore">
+          <p>Active</p>
+          <div>
+            <img
+              alt=""
+              src=""
+            />
+            <p>IEfja89uewaejwfawe0fjaewf98hwf8ejfhefwfebfaiuewfeuifh</p>
+            <button>Show Private Key</button>
+          </div>
+
+          <p>The active key is used to make transfers...</p>
+        </div>
+
+        <div className="keystore">
+          <p>Owner</p>
+          <div>
+            <img
+              alt=""
+              src=""
+            />
+            <p>IEfja89uewaejwfawe0fjaewf98hwf8ejfhefwfebfaiuewfeuifh</p>
+            <button>Show Private Key</button>
+          </div>
+
+          <p>The owner key is the master key...</p>
+          <p>The private key or password for the owner..</p>
+        </div>
+
+        <ResetPasswordForm />
       </div>
     );
   }
