99a21017ec225bc1f3c384dda014d3abd01eb47c
stubelshe <stu.belshe@tandem.ly>
2017-08-18 15:25:34 -0500

    Login styles and generic modal styles

--- src/components/forms/Login.js
+++ src/components/forms/Login.js
@@ -9,20 +9,22 @@ class Login extends Component {
 
     return (
       <form>
+        <p>This operation requires your Active Key or Master password.</p>
         <fieldset className="form-group">
           <label htmlFor="username">Username</label>
           <div clasName="input-container">
             <div className="icon" />
-
-            <input
-              aria-describedby="username"
-              className="form-control form-control-lg"
-              id="username"
-              name="username"
-              required
-              type="text"
-              value={username}
-            />
+            <div className="input-prefix">
+              <input
+                aria-describedby="username"
+                className="form-control form-control-lg prefix"
+                id="username"
+                name="username"
+                required
+                type="text"
+                value={username}
+              />
+            </div>
           </div>
         </fieldset>
 
@@ -40,26 +42,32 @@ class Login extends Component {
 
         <fieldset className="form-group">
           <label htmlFor="keepLoggedIn">Keep me logged in</label>
-          <input name="keepLoggedIn" type="checkbox" />
+          <input name="keepLoggedIn" type="checkbox" className="ml-2" />
         </fieldset>
 
-        <div className={`button-group ${modal && 'modal'}`}>
-          <button
-            className="btn btn-primary btn-lg"
-            type="submit"
-          >
-            Submit
-          </button>
-
-          {modal &&
+        <div className={`button-group ${modal && 'modal-cta'}`}>
+        <div className="row col-12 no-gutters p-0">
+          <div className="col-sm-auto col-12 pl-0 pr-sm-2 pr-0 mb-3">
             <button
-              className=""
-              onClick={handleClose}
+              className="btn btn-primary btn-lg btn-block"
+              type="submit"
             >
-              Cancel
-            </button>}
-
-          <Link to="/create-account">Don't have an account?</Link>
+              Login
+            </button>
+          </div>
+          <div className="col-sm-auto col-12 p2-2 pl-sm-2 pl-0 mb-3">
+            {modal &&
+              <button
+                className="btn btn-secondary btn-lg btn-block"
+                onClick={handleClose}
+              >
+                Cancel
+              </button>}
+            </div>
+            <div className="col-12 text-center text-sm-left">
+              <Link to="/create-account" className="text-link">Don't have an account?</Link>
+            </div>
+          </div>
         </div>
       </form>
     );
