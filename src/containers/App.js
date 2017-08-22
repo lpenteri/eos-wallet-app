068d043fb1a38f400763b3342f91adf78c9327fc
Orlando La <ceanela@users.noreply.github.com>
2017-08-22 10:53:02 -0500

    Remove <Redirect />\

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Redirect, Switch } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { Helmet } from 'react-helmet';
 import {
   Header,
