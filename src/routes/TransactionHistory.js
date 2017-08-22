6880f8f24c5bbc819194e2af9731b09369d7faa3
stubelshe <stu.belshe@tandem.ly>
2017-08-22 10:55:14 -0500

    Responsive content

--- src/routes/TransactionHistory.js
+++ src/routes/TransactionHistory.js
@@ -10,14 +10,12 @@ class TransactionHistory extends Component {
           <title>Transaction History</title>
         </Helmet>
         <div className="container-full">
-          <div className="d-flex justify-content-end">
-            <div className="mr-auto align-self-center">
+          <div className="d-md-flex justify-content-between items-center">
+            <div>
               <h2>Transaction History</h2>
             </div>
-            <div className="align-self-center">
+            <div className="d-flex items-center mb-2 mb-md-0">
               <div className="export icon-eos_icons_export" />
-            </div>
-            <div className="align-self-center">
               <div className="module-search">
                 <input
                   className="form-control"
